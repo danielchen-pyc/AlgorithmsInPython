1652cf32943616884b9a5db08e296d96a92eeca2
poyuchen <daniel7711potter@gmail.com>
2020-05-05 21:36:26 +0800

    Finished bubble sort algorithm.

--- bubblesort.py
+++ bubblesort.py
@@ -1,14 +1,13 @@
 def BubbleSort(list):
-    swap = False
-    while not swap:
-        for index in len(list) - 1:
+    swap = True
+    while swap:
+        swap = False
+        for index in range(len(list) - 1):
             num1 = list[index]
             num2 = list[index + 1]
-            if num2 > num1:
+            if num2 < num1:
                 list[index], list[index + 1] = num2, num1
                 swap = True
-
-
 
 
 

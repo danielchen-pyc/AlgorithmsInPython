4c1e4b9e54b7413ae9045552e9ba0424524dd5c7
poyuchen <daniel7711potter@gmail.com>
2020-05-06 10:02:38 +0800

    Binary search recursive method done.

--- binarysearch.py
+++ binarysearch.py
@@ -11,5 +11,19 @@ def binary_iter(target, list):
             stop = mid - 1
     return f"{target} not found in list"
 
+
+def binary_recur(target, list, start, stop):
+    if start > stop:
+        return f"{target} not found in list"
+    else:
+        mid = (start + stop)//2
+        if target == list[mid]:
+            return f"{target} found at index {mid}"
+        elif target > list[mid]:
+            return binary_recur(target, list, mid + 1, stop)
+        else:
+            return binary_recur(target, list, start, mid - 1)
+
+
 list = [4, 6, 2, 7, 3, 8, 3, 1, 11, 5, 9, 2]
-print(binary_iter(9, sorted(list)))
+print(binary_recur(9, sorted(list), 0, len(list) - 1))
